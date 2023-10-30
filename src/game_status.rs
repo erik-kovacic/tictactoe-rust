0253daca3dbb37b6927c8f28e5901e0a20d90e3e
Erik Kovačić <48680902+erik-kovacic@users.noreply.github.com>
2023-10-30 21:45:35 +0100

    Added dostring.

--- src/game_status.rs
+++ src/game_status.rs
@@ -1,5 +1,8 @@
 use crate::Player;
 
+/// Represents the current status of the Tic Tac Toe game.
+///
+/// The game can be in an `Ongoing` state, have a `Winner`, or result in a `Tie`.
 pub enum GameStatus {
     Ongoing,
     Winner(Player),
--- src/player.rs
+++ src/player.rs
@@ -1,11 +1,17 @@
 #[derive(Copy, Clone)]
 #[derive(Debug, PartialEq)]
 
+/// Represents a player in the Tic Tac Toe game.
+///
+/// A player can be either `X` or `O`.
 pub enum Player {
     X, O
 }
 
 impl Player {
+    /// Switches the current player to the other player.
+    ///
+    /// If the current player is `X`, they will become `O` and vice versa.
     pub fn switch(&mut self) {
         match *self {
             Player::X => *self = Player::O,
