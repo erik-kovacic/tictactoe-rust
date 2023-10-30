930de4ca327f3292bdb7be4c65488c3af9ea4b56
Erik Kovačić <48680902+erik-kovacic@users.noreply.github.com>
2023-10-30 21:28:33 +0100

    Code cleanup. Removed bad practices.
    Removed underscores from variable names.
    Added an option to play again.
    Cleaning the cli before displaying the board everytime.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::{self, Write};
+
 mod board;
 mod player;
 mod game_status;
@@ -7,25 +9,40 @@ use player::Player;
 use game_status::GameStatus;
 
 fn main() {
-    let mut _board = Board::new();
-    let mut _current_player = Player::X;
-    let mut _game_status = GameStatus::Ongoing;
+    loop {
+        let mut board = Board::new();
+        let mut current_player = Player::X;
+        let mut game_status = GameStatus::Ongoing;
 
-    
-    while let GameStatus::Ongoing = _game_status {
-        _board.display_board();
-        _board.make_move(_current_player);
+        while let GameStatus::Ongoing = game_status {
+            clear_screen();
+            board.display_board();
+            board.make_move(current_player);
         
-        _game_status = _board.check_game_status();
+            game_status = board.check_game_status();
     
-        if let GameStatus::Ongoing = _game_status {
-            _current_player.switch();
+            if let GameStatus::Ongoing = game_status {
+                current_player.switch();
+            }
+        }
+
+        match game_status {
+            GameStatus::Winner(player) => println!("{:?} is the winner!", player),
+            GameStatus::Tie => println!("It's a tie!"),
+            _ => {}
+        }
+
+        // Ask to play again
+        println!("Would you like to play again? (y/n)");
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        if input.trim().to_lowercase() != "y" {
+            break;
         }
     }
+}
 
-    match _game_status {
-        GameStatus::Winner(player) => println!("{:?} is the winner!", player),
-        GameStatus::Tie => println!("It's a tie!"),
-        _ => {}
-    }
+fn clear_screen() {
+    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+    io::stdout().flush().unwrap();
 }
