fc6a66adedda63950c646cca1717a5c2d9582b91
Erik Kovačić <48680902+erik-kovacic@users.noreply.github.com>
2023-10-30 21:43:48 +0100

    Added docstring.

--- src/board.rs
+++ src/board.rs
@@ -2,17 +2,24 @@ use crate::Player;
 use crate::GameStatus;
 use std::io;
 
+/// Represents the Tic Tac Toe game board.
+///
+/// The game board consists of a 3x3 grid, which can contain `Player::X`, `Player::O`, or be empty (None).
 pub struct Board {
     grid: [[Option<Player>; 3]; 3]
 }
 
 impl Board {
+    /// Creates a new, empty game board.
     pub fn new() -> Self {
         Board {
             grid: [[None; 3]; 3]
         }
     }
 
+    /// Displays the current state of the game board with borders.
+    ///
+    /// Players are represented as 'X' and 'O'. Empty cells are displayed with spaces.
     pub fn display_board(&self) {
         println!("+---+---+---+");
         for row in &self.grid {
@@ -30,10 +37,14 @@ impl Board {
         }
     }
 
+    /// Checks if the cell at the given row and column is empty.
     fn is_cell_empty(&self, row: usize, col: usize) -> bool {
         self.grid[row][col].is_none()
     }
 
+    /// Prompts the user for input and returns a valid usize (0, 1, or 2).
+    ///
+    /// Continues to prompt the user until a valid input is given.
     fn get_input() -> usize {
         loop {
             let mut input = String::new();
@@ -45,6 +56,10 @@ impl Board {
         }
     }
 
+    /// Allows the specified player to make a move on the board.
+    ///
+    /// Continues to prompt the player for a valid move until a valid and unoccupied cell is chosen.
+    /// Returns true once a valid move is made.    
     pub fn make_move(&mut self, player: Player) -> bool {
         loop {
             println!("Player {:?}, enter row (0, 1, 2): ", player);
@@ -61,6 +76,9 @@ impl Board {
         }
     }
 
+    /// Checks the current status of the game.
+    ///
+    /// Determines if the game has a winner, is a tie, or is still ongoing.
     pub fn check_game_status(&self) -> GameStatus {
         if let Some(winner) = self.has_winner() {
             GameStatus::Winner(winner)
@@ -71,6 +89,10 @@ impl Board {
         }
     }
 
+    /// Determines if there's a winner on the current board.
+    ///
+    /// Checks rows, columns, and diagonals to see if any player has three in a row.
+    /// Returns `Some(Player)` if there's a winner, and `None` otherwise.
     fn has_winner(&self) -> Option<Player> {
         for row in &self.grid {
             if let Some(&first) = row.first() {
@@ -100,6 +122,9 @@ impl Board {
         None
     }
 
+    /// Checks if the board is full, indicating a tie if there's no winner.
+    ///
+    /// Returns `true` if all cells are occupied, `false` otherwise.
     pub fn is_full(&self) -> bool {
         self.grid.iter().flatten().all(|&cell| cell.is_some())
     }
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,13 @@ use board::Board;
 use player::Player;
 use game_status::GameStatus;
 
+/// The main function that drives the Tic Tac Toe game.
+///
+/// It runs in a loop until the user decides to quit the game. Each loop:
+/// 1. Initializes a new game board.
+/// 2. Alternates between the two players allowing them to make a move.
+/// 3. Checks the game status after every move to determine if there's a winner or a tie.
+/// 4. Once a game ends, it prompts the user to play again or quit.
 fn main() {
     loop {
         let mut board = Board::new();
@@ -42,6 +49,10 @@ fn main() {
     }
 }
 
+/// Clears the terminal screen.
+///
+/// This function sends specific escape sequences to the terminal to clear it.
+/// It's useful to keep the game board view clean between the moves.
 fn clear_screen() {
     print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
     io::stdout().flush().unwrap();
